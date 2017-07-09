a71036c5e36e77c3eef5779bf5d2df6de9c3a172
dvoronov <voronov-d88@yandex.ru>
2017-07-09 12:55:54 +0300

    Init

---
+++ js/b-quote__btn-refresh.js
@@ -0,0 +1,27 @@
+function load_quotes(){
+    var quotes_dom = document.getElementById('quotes_json');
+    return JSON.parse(quotes_dom.innerText);
+}
+
+
+function get_random_quote(quotes){
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+
+function set_quote(quote){
+    var quote_text = document.getElementsByClassName('b-quote__text')[0];
+    var quote_author = document.getElementsByClassName('b-quote__author')[0];
+    quote_text.innerHTML = '<p>' + quote.phrase + '</p>';
+    if(quote.signature){
+        quote_author.innerHTML = '<p>©' + quote.signature + '</p>';
+    }else{
+        quote_author.innerHTML = '<p>©Неизвестный автор</p>';
+    }
+}
+
+
+function refresh_quote(){
+    var quotes = load_quotes();
+    set_quote(get_random_quote(quotes));
+}
